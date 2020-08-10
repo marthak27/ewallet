132e65896cc982e987dd0626bb0368391669fecd
martha <marthakristiana27@yahoo.com>
2020-08-10 19:07:36 +0700

    setting routes for login, regristration, crud user and logout

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+// $route['default_controller'] = 'welcome';
+// $route['404_override'] = '';
+// $route['translate_uri_dashes'] = FALSE;
+
+$route['api/authentication/login'] = 'api/authentication/login';
+$route['api/authentication/registration'] = 'api/authentication/registration';
+$route['api/authentication/user/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/authentication/user/id/$1/format/$3$4';
