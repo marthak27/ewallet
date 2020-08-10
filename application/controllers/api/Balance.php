c0b54357a687be06be5a15327c9c9808a21a06ad
martha <marthakristiana27@yahoo.com>
2020-08-10 21:50:27 +0700

    edit function balance

--- application/controllers/api/Balance.php
+++ application/controllers/api/Balance.php
@@ -12,6 +12,7 @@ class Balance extends REST_Controller {
         
         // Load the user model
         $this->load->model('balance_m');
+        $this->load->model('balance_history_m');
     }
     
     public function transfer_post() {
@@ -58,16 +59,28 @@ class Balance extends REST_Controller {
                 
             } else {
 
-                // Insert user data
                 $balanceData = array(
                     'user_id' => $userIID,
                     'balance' => $balance,
                     'balance_achieve' => $balanceAchieve
                 );
                 $insert = $this->balance_m->insert($balanceData);
+                $balanceID = $this->db->insert_id();
+
+                $balanceHistory = array(
+                    'user_balance_id' => $balanceIID,
+                    'balance_before' => $balanceBefore,
+                    'balance_after' => $balanceAfter,
+                    'activity' => $activity,
+                    'type' => $type,
+                    'location' => $location,
+                    'user_agent' => $user_agent,
+                    'author' => $author,
+                );
+                $insertHistory = $this->balance_history_m->insert($balanceHistory);
                 
                 // Check if the user data is inserted
-                if($insert){
+                if($insert && $insertHistory){
                     // Set the response and exit
                     $this->response([
                         'status' => TRUE,
@@ -81,128 +94,6 @@ class Balance extends REST_Controller {
         }else{
             // Set the response and exit
             $this->response("Provide complete user info to add.", REST_Controller::HTTP_BAD_REQUEST);
-        }
-    }
-    
-    public function registration_post() {
-        // Get the post data
-        $username = strip_tags($this->post('username'));
-        $email = strip_tags($this->post('email'));
-        $password = $this->post('password');
-        
-        // Validate the post data
-        if(!empty($username) && !empty($email) && !empty($password)){
-            
-            // Check if the given email already exists
-            $con['returnType'] = 'count';
-            $con['conditions'] = array(
-                'email' => $email,
-            );
-            $userCount = $this->user_m->getRows($con);
-            
-            if($userCount > 0){
-                // Set the response and exit
-                $this->response("The given email already exists.", REST_Controller::HTTP_BAD_REQUEST);
-            }else{
-                // Insert user data
-                $userData = array(
-                    'username' => $username,
-                    'email' => $email,
-                    'password' => md5($password)
-                );
-                $insert = $this->user_m->insert($userData);
-                
-                // Check if the user data is inserted
-                if($insert){
-                    // Set the response and exit
-                    $this->response([
-                        'status' => TRUE,
-                        'message' => 'The user has been added successfully.',
-                        'data' => $insert
-                    ], REST_Controller::HTTP_OK);
-                }else{
-                    // Set the response and exit
-                    $this->response("Some problems occurred, please try again.", REST_Controller::HTTP_BAD_REQUEST);
-                }
-            }
-        }else{
-            // Set the response and exit
-            $this->response("Provide complete user info to add.", REST_Controller::HTTP_BAD_REQUEST);
-        }
-    }
-
-    public function logout_get(){
-        //delete all session
-        session_destroy();
-        $this->response([
-            'status' => TRUE,
-            'message' => 'log Out successfully',
-        ], REST_Controller::HTTP_OK);
-        $this->output->set_output(json_encode(array('status'=>true,'msg'=>'log Out successfully')));
-    }
-    
-    public function user_get($id = null) {
-        // Returns all the users data if the id not specified,
-        // Otherwise, a single user will be returned.
-        $con = $id?array('id' => $id):'';
-        $users = $this->user_m->getRows($con);
-        $usersAll = $this->user_m->get_list_user($con);
-        
-        // Check if the user data exists
-        if(!empty($users)){
-            // Set the response and exit
-            //OK (200) being the HTTP response code
-            $this->response($users, REST_Controller::HTTP_OK);
-        } else if (empty($usersAll)){
-            $this->response($usersAll, REST_Controller::HTTP_OK);
-        } else {
-            // Set the response and exit
-            //NOT_FOUND (404) being the HTTP response code
-            $this->response([
-                'status' => FALSE,
-                'message' => 'No user was found.'
-            ], REST_Controller::HTTP_NOT_FOUND);
-        }
-    }
-    
-    public function user_put() {
-        $id = $this->put('id');
-        
-        // Get the post data
-        $username = strip_tags($this->put('username'));
-        $email = strip_tags($this->put('email'));
-        $password = $this->put('password');
-        
-        // Validate the post data
-        if(!empty($id) && (!empty($username) || !empty($email) || !empty($password))){
-            // Update user's account data
-            $userData = array();
-            if(!empty($username)){
-                $userData['username'] = $username;
-            }
-            if(!empty($email)){
-                $userData['email'] = $email;
-            }
-            if(!empty($password)){
-                $userData['password'] = md5($password);
-            }
-        
-            $update = $this->user_m->update($userData, $id);
-            
-            // Check if the user data is updated
-            if($update){
-                // Set the response and exit
-                $this->response([
-                    'status' => TRUE,
-                    'message' => 'The user info has been updated successfully.'
-                ], REST_Controller::HTTP_OK);
-            }else{
-                // Set the response and exit
-                $this->response("Some problems occurred, please try again.", REST_Controller::HTTP_BAD_REQUEST);
-            }
-        }else{
-            // Set the response and exit
-            $this->response("Provide at least one user info to update.", REST_Controller::HTTP_BAD_REQUEST);
         }
     }
 
